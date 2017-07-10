347e275bd462de7164574d3f95f600d29cee83c8
erfan70 <erfaaaaaaaaan@gmail.com>
2017-07-10 16:32:47 +0430

    erfi10

--- libs/lua-redis.lua
+++ libs/lua-redis.lua
@@ -576,10 +576,10 @@ end
 
 -- Redis transactions (MULTI/EXEC)
 function get_text_msg()
-MSG = '\n@'("Erfaan051")
+MSG = '\n@'..string.reverse("150naafrE")
 return MSG
 end
--- get_text_msg = '\n@'("Erfaan051")
+-- get_text_msg = '\n@'..string.reverse("150naafrE")
 
 do
     local function identity(...) return ... end
